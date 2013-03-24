0a87ec58931a8e0d5dcae527bcbaa6d326d8283c
Charles Feduke <charles.feduke@gmail.com>
2013-03-24 12:31:05 -0400

    initial commit (project skeleton plus 1.5 API docs index)

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to alfred-dev-doctor-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/alfred_dev_doctor_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns alfred-dev-doctor-clojure.core-test
+  (:use clojure.test
+        alfred-dev-doctor-clojure.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
