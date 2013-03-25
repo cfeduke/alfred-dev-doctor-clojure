9a6d702f00670c2377f46fa3f0f2267f1769eef1
Charles Feduke <charles.feduke@gmail.com>
2013-03-24 22:33:24 -0400

    completed v1.5 parser and output

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"] [org.clojure/data.json "0.2.1"]])
+  :dependencies [[org.clojure/clojure "1.4.0"] [org.clojure/data.json "0.2.1"]]
+  :main alfred-dev-doctor-clojure.core)
--- src/alfred_dev_doctor_clojure/core.clj
+++ src/alfred_dev_doctor_clojure/core.clj
@@ -1,5 +1,5 @@
 (ns alfred-dev-doctor-clojure.core)
-(use 'clojure.set)
+(use 'clojure.set 'clojure.java.io)
 (require '[clojure.data.json :as json])
 
 (defn deser-from-file*
@@ -15,10 +15,29 @@
 
 (def namespaces (:namespaces clj-index))
 
-(def namespaces-transform (rename (project namespaces [:name :wiki-url :doc]) {:name :title, :wiki-url :url, :doc :description}))
+(def vars (:vars clj-index))
+
+(defn transform 
+  "Transforms the CLJ index into a structure ammenable to the JSON format required."
+  [col]
+  (rename (project col [:name :wiki-url :doc]) {:name :title, :wiki-url :url, :doc :description}))
 
 (defn create-output-dir
   "Creates the ./output directory if it does not yet exist."
   []
   (let  [output-dir (java.io.File. "output")]
     (if (not (.exists output-dir)) (.mkdirs output-dir))))
+
+(defn write-json
+  "Outputs the structure as Alfred Dev Doctor compatible JSON in the output directory."
+  [col]
+  (with-open [w (writer (str "output" java.io.File/separator "data.clojure.json"))]
+                  (json/write col w)))
+
+(defn -main []
+  (create-output-dir)
+  (write-json
+    (transform
+      (into namespaces vars))))
+              
+                  
